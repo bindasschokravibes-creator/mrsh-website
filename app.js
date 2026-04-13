249b74fbd7fdbaef8b5c923abcf4fa696ec18738
bindasschokravibes-creator <bindasschokra.vibes@gmail.com>
2026-04-13 22:07:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+// static files
+app.use(express.static(__dirname));
+
+// home page
 app.get("/", (req, res) => {
-    res.send("HOME WORKING 💀");
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// dashboard
+app.get("/dashboard", (req, res) => {
+    res.sendFile(path.join(__dirname, "dashboard.html"));
+});
+
+// test
 app.get("/test", (req, res) => {
-    res.send("TEST WORKING 💀🔥");
+    res.send("EXPRESS WORKING 💀🔥");
 });
 
 app.listen(4000, () => {
-    console.log("SERVER RUNNING ON http://localhost:4000");
+    console.log("🔥 MRSH SERVER http://localhost:4000");
 });
