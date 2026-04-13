6d03e5d8d8394556b7f8d5a455d33e3e4fc1941a
bindasschokravibes-creator <bindasschokra.vibes@gmail.com>
2026-04-13 23:07:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
+// static files
 app.use(express.static(__dirname));
 
+// routes
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
@@ -17,6 +19,7 @@ app.get("/test", (req, res) => {
     res.send("TEST WORKING 💀🔥");
 });
 
+// start server
 app.listen(4000, () => {
     console.log("🚀 SERVER RUNNING: http://localhost:4000");
 });
